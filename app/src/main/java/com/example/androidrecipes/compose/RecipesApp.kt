ab808741cd39885ea9b6965531f9d784b1cf9a77
Aniokrait <itititcreclickhub@gmail.com>
2023-02-24 11:05:56 +0900

    文化遺産詳細に引数を渡せるところまで確認

--- app/src/main/java/com/example/androidrecipes/compose/RecipesApp.kt
+++ app/src/main/java/com/example/androidrecipes/compose/RecipesApp.kt
@@ -2,9 +2,12 @@ package com.example.androidrecipes.compose
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.androidrecipes.compose.heritages.HeritageDetailScreen
 import com.example.androidrecipes.compose.heritages.HeritageListScreen
 import com.example.androidrecipes.compose.home.HomeScreen
 
@@ -23,19 +26,19 @@ fun RecipesApp(
         composable("heritage_list") {
             HeritageListScreen(
                 navigateToHeritageDetail = {
-                    navController.navigate("heritage_detail/{heritageId}")
+                    navController.navigate("heritage_detail/$it")
                 }
             )
         }
-//        composable(
-//            "heritage_detail/{heritageId}",
-//            arguments = listOf(navArgument("heritageId") { type = NavType.IntType })
-//        ) { backStackEntry ->
-//            val arguments = requireNotNull(backStackEntry.arguments)
-//            HeritageDetail(
-//                recipeId = arguments.getInt("heritageId")
-//            )
-//        }
+        composable(
+            "heritage_detail/{heritageId}",
+            arguments = listOf(navArgument("heritageId") { type = NavType.IntType })
+        ) { backStackEntry ->
+            val arguments = requireNotNull(backStackEntry.arguments)
+            HeritageDetailScreen(
+                heritageId = arguments.getInt("heritageId")
+            )
+        }
 //        val uri = "https://androidrecipes.com"
 //        composable(
 //            "heritage_detail?heritageId={heritageId}",
