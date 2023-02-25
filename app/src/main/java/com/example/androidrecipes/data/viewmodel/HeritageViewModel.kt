e64a0417170edd55b9071fa921b297998aebcc61
Aniokrait <itititcreclickhub@gmail.com>
2023-02-25 13:35:52 +0900

    遅延こみのメソッドと遅延なしのメソッドで正しく表示されることの確認

--- app/src/main/java/com/example/androidrecipes/data/viewmodel/HeritageViewModel.kt
+++ app/src/main/java/com/example/androidrecipes/data/viewmodel/HeritageViewModel.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,8 +17,18 @@ class HeritageViewModel @Inject constructor(): ViewModel() {
     private val _description = mutableStateOf("")
     val description = _description
 
+    private val _quicDescription = MutableStateFlow("")
+    val quickDescription = _quicDescription.asStateFlow()
+
     init {
+        notDelayLoadDetail()
         delayLoadDetail()
+    }
+
+    private fun notDelayLoadDetail() {
+        CoroutineScope(Dispatchers.IO).launch {
+            _quicDescription.value = "This is a heritage site."
+        }
     }
 
     private fun delayLoadDetail() {
